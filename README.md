1c05a7bdddc9b14be273359b2fb04873b84562dc
Jenna Jay <jenneh83@gmail.com>
2024-01-11 22:48:46 -0600

    index file code updated to include png file and formatting for readme preview file

---
+++ README.md
@@ -0,0 +1,35 @@
+# ReadME
+
+## Description
+Create a professional readme using command line
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Inquirer 8.2.4 will be needed in your package json
+
+## Usage
+Run this program in node terminal
+
+## Screenshot
+![alt text](Screenshot.png)
+
+## License
+MIT
+
+## Contributing
+na
+
+## Tests
+na
+
+## Questions
+* GitHub: https://github.com/jennajay
+* Email: nope@gmail.com
--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+generateReadMe = ({ title, description, installation, usage, license, contribute, tests, questions, email }) =>
+`# ${title}
+
+## Description
+${description}
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Screenshot
+![alt text](Screenshot.png)
+
+## License
+${license}
+
+## Contributing
+${contribute}
+
+## Tests
+${tests}
+
+## Questions
+* GitHub: https://github.com/${questions}
+* Email: ${email}`;
+
 inquirer
 .prompt ([
     {
@@ -11,7 +49,7 @@ inquirer
     {
         type: 'input',
         name: 'description',
-        message: 'Provide a short description of your project.',
+        message: 'Provide a short summary of your project:',
     },
     {
        type: 'input',
@@ -21,22 +59,40 @@ inquirer
     {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions for use and/or include screenshots.',
-    },
-    {
-        type: 'input',
-        name: 'credit',
-        message: 'List your collaborators or sources, if any.',
-    },
-    {
-        type: 'input',
-        name: 'links',
-        message: 'List any links to, and for your project.',
+        message: 'Provide instructions or examples for use.',
     },
     {
         type: 'checkbox',
         name: 'license',
         message: 'Please select a license for this project.',
-        choices: ['MIT', 'Apache 2.0', 'GNU v3.0', 'Boost 1.0', 'BSD 2', 'BSD 3', 'none'],
-    }
+        choices: ['MIT', 'Apache 2.0', 'Boost 1.0', 'BSD 2', 'BSD 3', 'none'],
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'If you would like other developers to contribute, please provide any guidelines to do so.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'List any tests for your application.',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your GitHub username to link to your profile',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address for any additional questions',
+    },
 ])
+
+.then((response) => {
+    const readMeMD = generateReadMe(response);
+
+    fs.writeFile('README.md', readMeMD, (err) =>
+    err ? console.log(err) : console.log('Creating Professional README.md...')
+    );
+});
